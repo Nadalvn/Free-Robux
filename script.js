f1aa453014a479ce54f989d834114f98f625e242
Nadalvn <97375222+Nadalvn@users.noreply.github.com>
2022-08-09 09:06:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+console.log('You got scam!!!');
+console.log('You got scam!!!');
+console.log('You got scam!!!');
